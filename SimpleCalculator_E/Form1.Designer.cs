98dc790cb46a0196d2be5c8a9d4b5ce5f21ffeb8
Zohaib Shahid <zohaib_1991@hotmail.com>
2021-04-05 14:24:46 +0500

    Add project files.

---
+++ SimpleCalculator_E/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace SimpleCalculator_E
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.SubButton = new System.Windows.Forms.Button();
+            this.MulButton = new System.Windows.Forms.Button();
+            this.DivButton = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.RemButton = new System.Windows.Forms.Button();
+            this.SqRt = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(66, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 32);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(241, 38);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(100, 32);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Number 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(156, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Answer";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(142, 279);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(100, 32);
+            this.richTextBox3.TabIndex = 4;
+            this.richTextBox3.Text = "";
+            // 
+            // AddButton
+            // 
+            this.AddButton.Font = new System.Drawing.Font("Kristen ITC", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddButton.Location = new System.Drawing.Point(142, 153);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 52);
+            this.AddButton.TabIndex = 6;
+            this.AddButton.Text = "+";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // SubButton
+            // 
+            this.SubButton.Font = new System.Drawing.Font("Kristen ITC", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubButton.Location = new System.Drawing.Point(266, 95);
+            this.SubButton.Name = "SubButton";
+            this.SubButton.Size = new System.Drawing.Size(75, 52);
+            this.SubButton.TabIndex = 7;
+            this.SubButton.Text = "-";
+            this.SubButton.UseVisualStyleBackColor = true;
+            this.SubButton.Click += new System.EventHandler(this.SubButton_Click);
+            // 
+            // MulButton
+            // 
+            this.MulButton.Font = new System.Drawing.Font("Kristen ITC", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MulButton.Location = new System.Drawing.Point(49, 205);
+            this.MulButton.Name = "MulButton";
+            this.MulButton.Size = new System.Drawing.Size(75, 52);
+            this.MulButton.TabIndex = 8;
+            this.MulButton.Text = "x";
+            this.MulButton.UseVisualStyleBackColor = true;
+            this.MulButton.Click += new System.EventHandler(this.MulButton_Click);
+            // 
+            // DivButton
+            // 
+            this.DivButton.Font = new System.Drawing.Font("Kristen ITC", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DivButton.Location = new System.Drawing.Point(238, 205);
+            this.DivButton.Name = "DivButton";
+            this.DivButton.Size = new System.Drawing.Size(75, 52);
+            this.DivButton.TabIndex = 9;
+            this.DivButton.Text = "/";
+            this.DivButton.UseVisualStyleBackColor = true;
+            this.DivButton.Click += new System.EventHandler(this.DivButton_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // RemButton
+            // 
+            this.RemButton.Font = new System.Drawing.Font("Kristen ITC", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RemButton.Location = new System.Drawing.Point(37, 95);
+            this.RemButton.Name = "RemButton";
+            this.RemButton.Size = new System.Drawing.Size(87, 52);
+            this.RemButton.TabIndex = 10;
+            this.RemButton.Text = "Rem";
+            this.RemButton.UseVisualStyleBackColor = true;
+            this.RemButton.Click += new System.EventHandler(this.RemButton_Click);
+            // 
+            // SqRt
+            // 
+            this.SqRt.Font = new System.Drawing.Font("Kristen ITC", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SqRt.Location = new System.Drawing.Point(142, 95);
+            this.SqRt.Name = "SqRt";
+            this.SqRt.Size = new System.Drawing.Size(100, 52);
+            this.SqRt.TabIndex = 11;
+            this.SqRt.Text = "SqRt";
+            this.SqRt.UseVisualStyleBackColor = true;
+            this.SqRt.Click += new System.EventHandler(this.SqRt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.SqRt);
+            this.Controls.Add(this.RemButton);
+            this.Controls.Add(this.DivButton);
+            this.Controls.Add(this.MulButton);
+            this.Controls.Add(this.SubButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(500, 100);
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button SubButton;
+        private System.Windows.Forms.Button MulButton;
+        private System.Windows.Forms.Button DivButton;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button RemButton;
+        private System.Windows.Forms.Button SqRt;
+    }
+}
+
---
+++ SimpleCalculator_E/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator_E
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            float ans = 0, num1 = 0, num2 = 0;
+            num1 = float.Parse(richTextBox1.Text);
+            num2 = float.Parse(richTextBox2.Text);
+            ans = num1 + num2;
+            richTextBox3.Text = ans.ToString();
+
+        }
+
+        private void SubButton_Click(object sender, EventArgs e)
+        {
+            /*float ans = 0, num1 = 0, num2 = 0;
+            num1 = float.Parse(richTextBox1.Text);
+            num2 = float.Parse(richTextBox2.Text);
+            ans = num1 - num2;
+            richTextBox3.Text = ans.ToString();*/
+
+            richTextBox3.Text = (float.Parse(richTextBox1.Text) - float.Parse(richTextBox2.Text)).ToString();
+        }
+
+        private void MulButton_Click(object sender, EventArgs e)
+        {
+            /*float ans = 0, num1 = 0, num2 = 0;
+            num1 = float.Parse(richTextBox1.Text);
+            num2 = float.Parse(richTextBox2.Text);
+            ans = num1 * num2;
+            richTextBox3.Text = ans.ToString();*/
+
+            richTextBox3.Text = (float.Parse(richTextBox1.Text) * float.Parse(richTextBox2.Text)).ToString();
+        }
+
+        private void DivButton_Click(object sender, EventArgs e)
+        {
+            float ans = 0, num1 = 0, num2 = 0;
+            num1 = float.Parse(richTextBox1.Text);
+            num2 = float.Parse(richTextBox2.Text);
+            ans = num1 / num2;
+            richTextBox3.Text = ans.ToString();
+        }
+
+        private void RemButton_Click(object sender, EventArgs e)
+        {
+            float ans = 0, num1 = 0, num2 = 0;
+            num1 = float.Parse(richTextBox1.Text);
+            num2 = float.Parse(richTextBox2.Text);
+            ans = num1 % num2;
+            richTextBox3.Text = ans.ToString();
+        }
+
+        private void SqRt_Click(object sender, EventArgs e)
+        {
+            double ans = 0, num1 = 0;
+            num1 = double.Parse(richTextBox1.Text);
+            ans = Math.Sqrt(num1);
+            richTextBox3.Text = ans.ToString();
+        }
+    }
+}
